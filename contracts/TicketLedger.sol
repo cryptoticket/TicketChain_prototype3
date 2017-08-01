6bcce1266469454bbda2e60e2f78ab2cef12de32
KIrill Guzenko <kirill@chain.cloud>
2017-08-01 05:32:09 +0300

    contract fixes

--- contracts/TicketLedger.sol
+++ contracts/TicketLedger.sol
@@ -39,54 +39,72 @@ contract TicketLedger {
           creator = msg.sender;
      }
 
-     function getTicket(string id) constant returns (
+     function getTicketData1(string id) constant returns (
           int, string, int, 
           string, int, int, 
           string, string, string
-         // string, string, string, 
-         // string, int, string, 
-         // string
           ){
           Ticket t = tickets[id];
           return (t.status,      t.serial,        t.number, 
                   t.seat_sector, t.seat_row,      t.seat_number, 
                   t.customer,    t.customer_name, t.event_record
-                  //t.event_title, t.category,      t.category_name, 
-                 // t.order,       t.price,         t.price_currency, 
-                 // t.updated_at
                   );
      }
 
-     function setTicket(
+     function getTicketData2(string id) constant returns (
+          string, string, string, 
+          string, int,    string, 
+          string
+          ){
+          Ticket t = tickets[id];
+          return (t.event_title, t.category,      t.category_name, 
+                  t.order,       t.price,         t.price_currency, 
+                  t.updated_at
+                  );
+     }
+
+     function setTicketData1(
           string id,              int status_,         string serial_, 
           int number_,            string seat_sector_, int seat_row_, 
           int seat_number_,       string customer_,    string customer_name_, 
-          string event_//,          string event_title_, string category_, 
-          // string category_name_,  string order_,       int price_, 
-          // string price_currency_, string updated_at_
-          ){
+          string event_
+          ) returns (bool success){
           Ticket memory newTicket;
-          newTicket.status           = status_;
-          newTicket.serial           = serial_;
-          newTicket.number           = number_;
-          newTicket.seat_sector      = seat_sector_;
-          newTicket.seat_row         = seat_row_;
-          newTicket.seat_number      = seat_number_;
-          newTicket.customer         = customer_;
-          newTicket.customer_name    = customer_name_;
-          newTicket.event_record     = event_;
-          newTicket.event_title      = "event_title_";
-          newTicket.category         = "category_";
-          newTicket.category_name    = "category_name_";
-          newTicket.order            = "order_";
-          newTicket.price            = 100500;
-          newTicket.price_currency   = "price_currency_";
-          newTicket.updated_at       = "updated_at_";
-        
+          newTicket.status         = status_;
+          newTicket.serial         = serial_;
+          newTicket.number         = number_;
+          newTicket.seat_sector    = seat_sector_;
+          newTicket.seat_row       = seat_row_;
+          newTicket.seat_number    = seat_number_;
+          newTicket.customer       = customer_;
+          newTicket.customer_name  = customer_name_;
+          newTicket.event_record   = event_;
+
           tickets[id] = newTicket;
+          success = true;
           return;
      }
      
+     function setTicketData2(
+          string id,              string event_title_, string category_, 
+          string category_name_,  string order_,       int price_,       
+          string price_currency_, string updated_at_
+          ) returns (bool success){
+          Ticket t = tickets[id];
+
+          t.event_title    = event_title_;
+          t.category       = category_;
+          t.category_name  = category_name_;
+          t.order          = order_;
+          t.price          = price_;
+          t.price_currency = price_currency_;
+          t.updated_at     = updated_at_;
+        
+          tickets[id] = t;
+          success = true;
+          return;
+     }
+
      function (){
           throw;
      }
